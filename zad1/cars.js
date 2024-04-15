effedd1ef175ecba4b8a537e71ff1de5eecfe23d
darasviryd <161596387+darasviryd@users.noreply.github.com>
2024-04-15 22:17:14 +0200

    Add files via upload

---
+++ zad1/cars.js
@@ -0,0 +1,9 @@
+const cars = [
+    { id: 1, make: "Toyota", model: "Yaris", year: 2001, color: "white" },
+    { id: 2, make: "Honda", model: "Civic", year: 2010, color: "blue" },
+    { id: 3, make: "Ford", model: "Focus", year: 2015, color: "black" },
+    { id: 4, make: "Chevrolet", model: "Cruze", year: 2018, color: "silver" },
+    { id: 5, make: "Volkswagen", model: "Golf", year: 2019, color: "red" }
+];
+
+module.exports = cars;
