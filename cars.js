7875e47da76d0b96ed337e8de41c54436ca2ed59
darasviryd <161596387+darasviryd@users.noreply.github.com>
2024-04-22 20:42:24 +0200

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,29 @@
+// cars.js
+
+const cars = [
+    { id: 1, make: "Tesla", model: "Model S", year: 2023, color: "black" },
+    { id: 2, make: "Porsche", model: "911", year: 2022, color: "blue" },
+    { id: 3, make: "Lamborghini", model: "Huracan", year: 2024, color: "yellow" },
+    { id: 4, make: "Ferrari", model: "488 GTB", year: 2021, color: "red" },
+    { id: 5, make: "McLaren", model: "720S", year: 2023, color: "orange" }
+];
+
+function getCars() {
+    return cars;
+}
+
+function getCarInformation(id) {
+    const car = cars.find(car => car.id === id);
+    if (!car) return "Car doesn't exist";
+    return `Make: ${car.make}, Model: ${car.model}, Year: ${car.year}, Color: ${car.color}.`;
+}
+
+function getCarAge(id) {
+    const car = cars.find(car => car.id === id);
+    if (!car) return "Car doesn't exist";
+    const currentYear = new Date().getFullYear();
+    const carAge = currentYear - car.year;
+    return `Car is ${carAge} years old.`;
+}
+
+module.exports = { getCars, getCarInformation, getCarAge };
---
+++ htmlGenerator.js
@@ -0,0 +1,18 @@
+// htmlGenerator.js
+
+const htmlGenerator = {
+    getHTMLDocumentStart: function() {
+        return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cars</title>
+</head>`;
+    },
+    getHTMLDocumentEnd: function() {
+        return `</html>`;
+    }
+};
+
+module.exports = htmlGenerator;
